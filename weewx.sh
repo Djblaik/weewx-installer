9876f6acb4723b62720bf1364c74efd1cc524025
David Blaik <blaikey@live.com.au>
2023-05-10 13:08:33 +1000

    update

--- weewx.sh
+++ weewx.sh
@@ -38,7 +38,7 @@ sudo update-rc.d weewx defaults 98
 
 #install belchertown skin
 wget https://github.com/poblabs/weewx-belchertown/releases/download/weewx-belchertown-1.3.1/weewx-belchertown-release.1.3.1.tar.gz
-sudo /home/weewx/bin/wee_extension --install weewx-belchertown-release-1.3.1.tar.gz
+sudo /home/weewx/bin/wee_extension --install weewx-belchertown-release.1.3.1.tar.gz
 
 #install nginx
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
