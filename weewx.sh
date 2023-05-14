d9fd4522ab180f0e54946d49409eb25d7c0898e7
David Blaik <blaikey@live.com.au>
2023-05-14 19:34:15 +1000

    Update weewx.sh

--- weewx.sh
+++ weewx.sh
@@ -36,25 +36,27 @@ sudo cp /home/weewx/util/init.d/weewx.debian /etc/init.d/weewx
 sudo chmod +x /etc/init.d/weewx
 sudo update-rc.d weewx defaults 98
 
-#install weather34 skin
-sudo apt install php8.1
-sudo apt install php8.1-cli php8.1-fpm php8.1-sqlite3 php8.1-zip php8.1-gd  php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
-sudo a2enmod php8.1
-wget https://github.com/steepleian/weewx-Weather34/archive/refs/heads/main.zip
-wget https://raw.githubusercontent.com/Djblaik/weewx-installer/weewx-weather34/services.txt
-sudo apt install unzip
-unzip main.zip
-cd weewx-Weather34-main
-sudo python3 w34_installer.py
-sudo systemctl restart weewx
-sudo python3 ./home/weewx/bin/wee_reports
-
 #install nginx
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
 sudo apt update
 sudo apt install nginx
-wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/main/nginx.conf
-sudo mv nginx.conf /etc/nginx/
-sudo systemctl stop nginx
-sudo systemctl start nginx
-sudo systemctl start weewx
+wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/weewx-weather34/weewx
+sudo mv weewx /etc/nginx/sites-available
+sudo cp /etc/nginx/sites-available/weewx /etc/nginx/sites-enabled/weewx
+sudo systemctl restart nginx
+
+#install weather34 skin
+sudo apt install php8.1-fpm php8.1-cli php8.1-sqlite3 php8.1-zip php8.1-gd  php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
+wget https://github.com/steepleian/weewx-Weather34/archive/refs/heads/main.zip
+unzip main.zip
+wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/weewx-weather34/services.txt
+wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/weewx-weather34/setup_py.conf
+sudo rm weewx-Weather34-main/setup_py.conf
+sudo mv setup_py.conf weewx-Weather34-main
+sudo systemctl restart nginx
+sudo apt install unzip
+cd weewx-Weather34-main
+sudo python3 w34_installer.py
+sudo systemctl restart weewx
+cd ..
+sudo /home/weewx/bin/wee_reports
