bc6b939011f46ef0a94667302022a680d9d6d605
David Blaik <blaikey@live.com.au>
2023-05-14 13:17:15 +1000

    Update w34skinonly.sh

--- w34skinonly.sh
+++ w34skinonly.sh
@@ -1,13 +1,17 @@
 #install weather34 skin
 sudo apt install php8.1-fpm php8.1-cli php8.1-sqlite3 php8.1-zip php8.1-gd  php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
 wget https://github.com/steepleian/weewx-Weather34/archive/refs/heads/main.zip
+unzip main.zip
 wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/weewx-weather34/services.txt
 wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/weewx-weather34/nginx.conf
+wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/weewx-weather34/setup_py.conf
 sudo mv nginx.conf /etc/nginx/
+sudo rm /weewx-Weather34-main/setup_py.conf
+sudo mv setup_py.conf /weewx-Weather34-main
 sudo systemctl restart nginx
 sudo apt install unzip
-unzip main.zip
 cd weewx-Weather34-main
 sudo python3 w34_installer.py
 sudo systemctl restart weewx
+cd ..
 sudo python3 ./home/weewx/bin/wee_reports
