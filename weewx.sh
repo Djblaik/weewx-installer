a2e15131b314a3a3b92520d0e7b5eb7fcd0890c1
David Blaik <blaikey@live.com.au>
2023-05-14 10:55:14 +1000

    Update weewx.sh

--- weewx.sh
+++ weewx.sh
@@ -46,5 +46,6 @@ sudo apt update
 sudo apt install nginx
 wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/main/nginx.conf
 sudo mv nginx.conf /etc/nginx/
+sudo systemctl stop nginx
 sudo systemctl start nginx
 sudo systemctl start weewx
