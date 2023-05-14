b930db93a03b3cd0f5375bb44f3cc66fca2e7c4f
David Blaik <blaikey@live.com.au>
2023-05-14 12:42:07 +1000

    Update w34skinonly.sh

--- w34skinonly.sh
+++ w34skinonly.sh
@@ -1,5 +1,5 @@
 #install weather34 skin
-sudo apt install php8.1-cli php8.1-fpm php8.1-sqlite3 php8.1-zip php8.1-gd  php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
+sudo apt install php8.1-fpm php8.1-cli php8.1-sqlite3 php8.1-zip php8.1-gd  php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
 wget https://github.com/steepleian/weewx-Weather34/archive/refs/heads/main.zip
 wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/weewx-weather34/services.txt
 wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/weewx-weather34/nginx.conf
