ab3cf2a3d714ae1dc627d2a52ee6454099c84e5e
David Blaik <blaikey@live.com.au>
2023-05-16 16:17:31 +1000

    Update installweewx.sh

--- installweewx.sh
+++ installweewx.sh
@@ -34,12 +34,12 @@ sudo rm $filename
 wget --header 'Authorization: token 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' https://raw.githubusercontent.com/Djblaik/weewx-installer/main/weewx.conf
 sudo rm /home/weewx/weewx.conf
 sudo mv weewx.conf /home/weewx
-sudo systemctl start weewx
 
 #run as daemon automatically when the computer starts
 sudo cp /home/weewx/util/init.d/weewx.debian /etc/init.d/weewx
 sudo chmod +x /etc/init.d/weewx
 sudo update-rc.d weewx defaults 98
+sudo systemctl start weewx
 
 #install weewx interceptor
 wget -O weewx-interceptor.zip https://github.com/matthewwall/weewx-interceptor/archive/master.zip
