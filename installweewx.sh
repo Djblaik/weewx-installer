e8497f92f6f64a48cb30fa891875a75a04fd1cdd
David Blaik <blaikey@live.com.au>
2023-05-16 14:16:40 +1000

    Update installweewx.sh

--- installweewx.sh
+++ installweewx.sh
@@ -63,6 +63,7 @@ sudo rm weewx
 
 #install weather34 skin and php8.1-fpm
 weather34 () {
+sudo apt install php8.1-fpm php8.1-cli php8.1-sqlite3 php8.1-zip php8.1-gd  php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
 file=$(curl -Ls https://api.github.com/repos/steepleian/weewx-Weather34/releases/latest | grep tarball_url | sed -re 's/.*: "([^"]+)".*/\1/')
 filename=$(basename "$file")
 echo $filename
