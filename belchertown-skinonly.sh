d498fc2e982de32985e07353f1a92a5078036ece
David Blaik <blaikey@live.com.au>
2023-05-16 14:21:56 +1000

    Update belchertown-skinonly.sh

--- belchertown-skinonly.sh
+++ belchertown-skinonly.sh
@@ -3,9 +3,6 @@ set -e
 #install belchertown skin
 file=$(curl -Ls https://api.github.com/repos/poblabs/weewx-belchertown/releases/latest | grep tarball_url | sed -re 's/.*: "([^"]+)".*/\1/')
 filename=$(basename "$file")
-echo $filename
-echo $file
-echo ${file##*/}
 wget $file
 sudo mv $filename "${filename}.tar.gz"
 filename="${filename}.tar.gz"
