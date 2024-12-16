6838507573c6fd28355668b0b34a13f0596326c9
David Blaik <20061763+Djblaik@users.noreply.github.com>
2024-12-17 09:28:18 +1000

    Update installweewx.sh

--- installweewx.sh
+++ installweewx.sh
@@ -34,8 +34,8 @@ wget https://raw.githubusercontent.com/Djblaik/weewx-installer/main/weewx.conf
 sudo rm ~/weewx-data/weewx.conf
 sudo mv weewx.conf ~/weewx-data
 #install lowBattery.py
-wget https://raw.githubusercontent.com/Djblaik/weewx-installer/main/LowBattery.py
-sudo mv LowBattery.py ~/weewx-data/bin/user
+wget https://raw.githubusercontent.com/Djblaik/weewx-installer/main/LowBatteryPush.py
+sudo mv LowBatteryPush.py ~/weewx-data/bin/user
 #run weewx as a daemon
 sudo sh ~/weewx-data/scripts/setup-daemon.sh
 sudo systemctl start weewx
