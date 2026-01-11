2f26031168286b17b4c1b30cdfeb63dc5445f696
David Blaik <20061763+Djblaik@users.noreply.github.com>
2026-01-12 09:13:48 +1000

    revert folder to belchertown

--- installweewx.sh
+++ installweewx.sh
@@ -89,9 +89,8 @@ wget https://raw.githubusercontent.com/Djblaik/weewx-installer/main/graphs.conf
 sudo mv graphs.conf ~/weewx-data/skins/Belchertown
 wget https://raw.githubusercontent.com/Djblaik/weewx-installer/main/index_hook_after_station_info.inc
 sudo mv index_hook_after_station_info.inc ~/weewx-data/skins/Belchertown
-sudo mv ~/weewx-data/public_html/belchertown ~/weewx-data/public_html/southgladstone
 wget https://raw.githubusercontent.com/Djblaik/weewx-installer/main/bomicons.tar
-tar -xf bomicons.tar -C ~/weewx-data/public_html/southgladstone
+tar -xf bomicons.tar -C ~/weewx-data/public_html/belchertown
 
 sudo systemctl restart weewx
 
